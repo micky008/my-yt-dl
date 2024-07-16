571ae22064e5ca62b06be49020f069bdcd691daa
mchinchole <michael.chinchole@thalesgroup.com>
2024-07-16 16:28:35 +0200

    init commit

---
+++ src/main/java/com/msc/my/yt/dl2/MyYtDl2.java
@@ -0,0 +1,15 @@
+package com.msc.my.yt.dl2;
+
+import com.msc.my.yt.dl2.test.YtDLP_GetInfo;
+
+/**
+ *
+ * @author mchinchole
+ */
+public class MyYtDl2 {
+
+    public static void main(String[] args) {
+        YtDLP_GetInfo y = new YtDLP_GetInfo();
+        y.run();
+    }
+}
---
+++ src/main/java/com/msc/my/yt/dl2/test/YtDLP.java
@@ -0,0 +1,14 @@
+package com.msc.my.yt.dl2.test;
+
+import java.util.List;
+
+/**
+ *
+ * @author mchinchole
+ */
+public class YtDLP {
+
+    public String title;
+    public List<Format> formats;
+
+}
